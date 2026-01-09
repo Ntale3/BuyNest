733207def63e5b0e88c62d6f9f056fe4e9ecef92
Ntale Samad <swamaduntale98@gmail.com>
2026-01-09 20:40:20 +0300

    ✨ add themeprovider

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,9 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { ThemeProvider } from "@/components/theme-provider"
 
 const RootLayout = () => (
-  <>
+  <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
     <div className="p-2 flex gap-2">
       <Link to="/" className="[&.active]:font-bold">
         Home
@@ -14,7 +15,7 @@ const RootLayout = () => (
     <hr />
     <Outlet />
     <TanStackRouterDevtools />
-  </>
+  </ThemeProvider>
 )
 
 export const Route = createRootRoute({ component: RootLayout })
