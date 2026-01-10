d9a4cc6d3e8bd85ee061b8ac4f44da5127087109
Ntale Samad <swamaduntale98@gmail.com>
2026-01-10 20:42:30 +0300

    ❇️  add dummy assets

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -2,10 +2,18 @@ import logo from "./logo.svg"
 import sony from "./sony_airbuds_image.png"
 import star_icon from "./star_icon.svg";
 import star_dull_icon from "./star_dull_icon.svg";
+import header_headphone_image from "./header_headphone_image.png";
+import header_playstation_image from "./header_playstation_image.png";
+import header_macbook_image from "./header_macbook_image.png";
+import arrow_icon from "./arrow_icon.svg"
 
 export  const assets={
 logo,
 sony,
 star_dull_icon,
-star_icon
+star_icon,
+header_headphone_image,
+header_playstation_image,
+header_macbook_image,
+arrow_icon
 }
