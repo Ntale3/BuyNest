948ba65ff4e5b0b9101f070dd1a71de98ffdf3c7
Ntale Samad <swamaduntale98@gmail.com>
2026-01-10 14:13:18 +0300

    ✨ create sidebar component

---
+++ src/components/ui/sidebar.tsx
@@ -0,0 +1,50 @@
+import useSideBarStore from "@/store/navigation"
+import { Link, linkOptions } from "@tanstack/react-router"
+
+const options = linkOptions([
+  {
+    to: '/home' as string,
+    label: 'Home',
+    activeOptions: { exact: true },
+  },
+  {
+    to: '/shop' as string,
+    label: 'Shop',
+  },
+  {
+    to: '/about' as string,
+    label: 'About',
+  },
+  {
+    to:'/contact' as string,
+    label:'Contact'
+  },
+  {
+    to:'/cart' as string,
+    label:'Cart'
+  }
+])
+const Sidebar = () => {
+const {toggleSideBar} = useSideBarStore()
+  return (
+    <aside className="fixed top-0 right-0 z-40 w-64 h-full transition-transform  md:hidden">
+      <div className="h-full px-3 py-4 overflow-y-auto bg-sidebar border-e border-default flex flex-col">
+        {options.map((option) => {
+                  return (
+                    <Link
+                      {...option}
+                      key={option.to}
+                      activeProps={{ className: `underline underline-offset-8 decoration-2 decoration-orange-800` }}
+                      className="p-2 text-lg space-y-2 font-medium"
+                      onClick={toggleSideBar}
+                    >
+                      {option.label}
+                    </Link>
+                  )
+                })}
+      </div>
+    </aside>
+  )
+}
+
+export default Sidebar
