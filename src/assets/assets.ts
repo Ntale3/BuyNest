d2fbe5faa6caf3c5af5961f15d793b1e1c03eea6
Ntale Samad <swamaduntale98@gmail.com>
2026-01-10 19:23:53 +0300

    ❇️  add dummy assets

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,5 +1,11 @@
 import logo from "./logo.svg"
+import sony from "./sony_airbuds_image.png"
+import star_icon from "./star_icon.svg";
+import star_dull_icon from "./star_dull_icon.svg";
 
 export  const assets={
 logo,
+sony,
+star_dull_icon,
+star_icon
 }
